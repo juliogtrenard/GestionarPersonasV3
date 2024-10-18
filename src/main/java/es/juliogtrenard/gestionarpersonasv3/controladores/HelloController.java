074a9fe9d8ecc4b673367f44dd53399c8a6a1222
Julio <juliogtrenard@gmail.com>
2024-10-18 13:54:09 +0200

    ADD controladores de la ventana principal y modal

--- src/main/java/es/juliogtrenard/gestionarpersonasv3/controladores/HelloController.java
+++ src/main/java/es/juliogtrenard/gestionarpersonasv3/controladores/HelloController.java
@@ -1,15 +1,17 @@
 package es.juliogtrenard.gestionarpersonasv3.controladores;
 
+import es.juliogtrenard.gestionarpersonasv3.modelos.Persona;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  * Controlador para la interfaz gráfica de la gestión de personas.
@@ -17,6 +19,24 @@ import java.util.Objects;
  * validando la entrada del usuario y actualizando la vista.
  */
 public class HelloController {
+    /**
+     * Lista de {@link Persona} que se muestra en la tabla.
+     */
+    public static ArrayList<Persona> listaPersonas;
+
+    /**
+     * Tabla que muestra la lista de personas.
+     */
+    @FXML
+    private TableView<Persona> tvTabla;
+
+    /**
+     * Inicializa la lista de personas.
+     */
+    @FXML
+    public void initialize() {
+        listaPersonas = new ArrayList<>();
+    }
 
     /**
      * Maneja el evento de agregar una nueva persona a la lista.
@@ -27,7 +47,9 @@ public class HelloController {
     @FXML
     public void agregarPersona(ActionEvent event) throws IOException {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/es/juliogtrenard/gestionarpersonasv3/modal.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/es/juliogtrenard/gestionarpersonasv3/modal.fxml"));
+        Parent root = loader.load();
+
         stage.setScene(new Scene(root));
         stage.setTitle("Nueva Persona");
 
@@ -38,7 +60,14 @@ public class HelloController {
 
         stage.initModality(Modality.WINDOW_MODAL);
         stage.initOwner(
-                ((Node)event.getSource()).getScene().getWindow() );
-        stage.show();
+                ((Node)event.getSource()).getScene().getWindow());
+        stage.showAndWait();
+
+        ControladorModal controller = loader.getController();
+        Persona persona = controller.getPersona();
+        if (persona != null) {
+            listaPersonas.add(persona);
+            tvTabla.getItems().add(persona);
+        }
     }
 }
